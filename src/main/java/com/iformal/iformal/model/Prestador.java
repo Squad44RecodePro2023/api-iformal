1fe8490e98bcb5d852fdea3c65b4944e017ff38e
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-01-16 14:44:34 -0300

    update: Adicionada relacionamentos

--- src/main/java/com/iformal/iformal/model/Prestador.java
+++ src/main/java/com/iformal/iformal/model/Prestador.java
@@ -1,10 +1,14 @@
 package com.iformal.iformal.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,9 +28,16 @@ public class Prestador {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column( nullable = false, length = 11)
+    @Column( nullable = false, length = 11, columnDefinition = "CHAR(11)")
     private String contato;
     
+    @Column (nullable = false, length = 255)
+    private String especialidades;
 
+    @OneToOne
+    private Usuarios usuario;
+
+     @OneToMany(mappedBy = "prestador")
+    private List<Avaliacoes> avalicaoes;
     
 }
