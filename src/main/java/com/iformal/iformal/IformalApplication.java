d5311a462a3b22b50f277993e2ed3c1c60538dd4
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-01-16 11:03:25 -0300

    git init

---
+++ src/main/java/com/iformal/iformal/IformalApplication.java
@@ -0,0 +1,13 @@
+package com.iformal.iformal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IformalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IformalApplication.class, args);
+	}
+
+}
