25ce4a845f0e2a6f763028373a0d0210c9dae60e
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-01-16 14:16:50 -0300

    feat: modelagem inicial da tabela avaliacoes

---
+++ src/main/java/com/iformal/iformal/model/Avaliacoes.java
@@ -0,0 +1,31 @@
+package com.iformal.iformal.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="avaliacao_prestador")
+@Getter 
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Avaliacoes {
+    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column( length = 255)
+    private String comentario;
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private int nota;
+    
+}
