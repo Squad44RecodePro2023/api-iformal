56d1470f486613573a4e346eb504d29dceb77034
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-01-16 15:41:35 -0300

    feat: Criação da entidade serviços

---
+++ src/main/java/com/iformal/iformal/model/Servicos.java
@@ -0,0 +1,35 @@
+package com.iformal.iformal.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="servicos")
+@Getter 
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Servicos {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column( nullable = false, length = 55, unique = true)
+    private String nome;
+    @Column( nullable = false, length = 255)
+    private String descricao;
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categorias categoria ;
+    
+}
