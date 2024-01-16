a9648f9852a79a0e5b4d279b65a344c09d7faad9
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-01-16 14:17:00 -0300

    feat: modelagem inicial da tabela comentarios

---
+++ src/main/java/com/iformal/iformal/model/Comentarios.java
@@ -0,0 +1,29 @@
+package com.iformal.iformal.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="comentarios")
+@Getter 
+@Setter
+@ToString
+@AllArgsConstructor
+public class Comentarios {
+     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;    
+    @Column( nullable = false, length = 255)
+    private String comentario;
+    @Column (nullable = false, length = 55)
+    private String autor;
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private int nota;
+}
