f890d20fbb1de98a4e3c421714e75e2623ffc53e
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-01-16 21:50:32 -0300

    feat: Adiciona classe DTO para Categoria

---
+++ src/main/java/com/iformal/iformal/dto/CategoriasDto.java
@@ -0,0 +1,15 @@
+package com.iformal.iformal.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CategoriasDto(
+    @NotBlank(message = "O nome não pode estar vazio") 
+    @Size(max = 55, message = "O nome deve ter no máximo 55 caracteres") 
+    String nome,
+    @NotBlank(message = "A descrição não pode estar vazia") 
+    @Size(max = 255, message = "A descrição deve ter no máximo 255 caracteres") 
+    String descricao
+    ) {
+    
+}
