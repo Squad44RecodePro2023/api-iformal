d773f8bd21cb7d9210eb435118223f32b06abf18
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-01-16 21:50:41 -0300

    feat: Adiciona classe DTO para Certificado

---
+++ src/main/java/com/iformal/iformal/dto/CertificadoDto.java
@@ -0,0 +1,19 @@
+package com.iformal.iformal.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record CertificadoDto(
+      @NotBlank(message = "O nome não pode estar vazio") 
+      @Size(max = 55, message = "O nome deve ter no máximo 55 caracteres") 
+      String nome,
+      @NotBlank(message = "A descrição não pode estar vazia") 
+      @Size(max = 255, message = "A descrição deve ter no máximo 255 caracteres") 
+      String descricao,
+      @NotNull(message = "O prestador não pode ser nulo") 
+      Long prestadorId
+) {
+
+    
+}
