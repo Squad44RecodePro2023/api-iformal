a57f875d58d3be13229ba920f5f6ceb57d91a900
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2024-01-19 01:45:45 -0300

    Feat Avaliação Controller

---
+++ src/main/java/com/iformal/iformal/controller/AvaliacaoController.java
@@ -0,0 +1,76 @@
+package com.iformal.iformal.controller;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iformal.iformal.dto.AvaliacaoesDto;
+import com.iformal.iformal.model.Avaliacoes;
+import com.iformal.iformal.services.impl.AvaliacoesService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/api/avalicaoes")
+public class AvaliacaoController {
+
+    @Autowired
+    AvaliacoesService avaliacoesService;
+
+    @Operation( summary = "Retorna uma avaliação pelo id")
+    @GetMapping("/{id}")
+    public ResponseEntity<Avaliacoes> getbyId(@PathVariable (value = "id") int id){        
+        var avaliacaoList = this.avaliacoesService.listById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(avaliacaoList);
+    }
+    
+    @Operation(summary="Retorna uma lista de avaliações pelo id do prestador")
+    @GetMapping("/prestador/{id}")
+    public ResponseEntity<List<Avaliacoes>> getAllByPrestador(@PathVariable (value = "id") int id){        
+        var avaliacaoList = this.avaliacoesService.listAllByPrestador(id);
+        return ResponseEntity.status(HttpStatus.OK).body(avaliacaoList);
+    }
+    
+    @Operation(summary="Retorna uma lista de avaliações pelo id do usuario")
+    @GetMapping("/usuario/{id}")
+    public ResponseEntity<List<Avaliacoes>> getAllByUsuario(@PathVariable (value = "id") int id){        
+        var avaliacaoList = this.avaliacoesService.listAllByUsuario(id);
+        return ResponseEntity.status(HttpStatus.OK).body(avaliacaoList);
+    }
+    
+
+    @Operation(summary = "Deleta uma avaliação pelo id")
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable (value = "id") int id){
+        this.avaliacoesService.delete(id);
+    }
+
+    @Operation(summary = "Salva uma Avaliação no banco de dados")
+    @PostMapping()
+    public ResponseEntity<Avaliacoes> save(@RequestBody @Valid AvaliacaoesDto dto) {    
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.avaliacoesService.save(dto));
+    }
+
+
+    @Operation(summary = "Atualiza uma Avaliação no banco de dados")
+    @PutMapping()
+    public ResponseEntity<Avaliacoes> update(@PathParam (value = "id") int id, @RequestBody @Valid AvaliacaoesDto dto) {    
+        return ResponseEntity.status(HttpStatus.OK).body(this.avaliacoesService.update(dto, id));
+    }
+
+    
+}
